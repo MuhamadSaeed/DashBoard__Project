3522dc0eede26188e81daa45b6600f71657ede78
MuhamadSaeed <mohamedsaiedz2156287@gmail.com>
2022-04-28 17:42:27 +0200

    Upload the Project

---
+++ js/main.js
@@ -0,0 +1,8 @@
+{document.querySelector("aside .mid .arrow").addEventListener("click", () => {
+    document.querySelector("aside .mid .arrow .af").classList.toggle("rotate-45-2")
+    document.querySelector("aside .mid .arrow .be").classList.toggle("rotate--45-2")
+    document.querySelector("aside .content-btn ul").classList.toggle("block")
+})};
+{document.querySelector("header .main-i").addEventListener("click", () => {
+    document.querySelector("aside").classList.toggle("aside-991")
+})};
